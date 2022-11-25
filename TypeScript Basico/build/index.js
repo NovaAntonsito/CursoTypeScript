60fa8dfd79cfbfd4b79c21dfff7713f0a7c7744a
Marcos Anton <marcos.anton@alumnos.frm.utn.edu.ar>
2022-11-24 23:58:11 -0300

    Introduccion a las variables y funciones

--- TypeScript Basico/build/index.js
+++ TypeScript Basico/build/index.js
@@ -1,2 +1,92 @@
 "use strict";
-console.log("Hola Nodemon");
+//Declaracion de variables
+let nombre = 'Marcos';
+let edad = 23;
+let email = 'antonbarroso16@gmail.com';
+let empleado = false;
+let comodin = 'Soy lo que sea'; //Cambia a todo tipo de variable :Any
+//Console.log
+console.log('Hola ' + nombre + ' tienes ' + edad + ' y tu email es: ' + email);
+console.log(empleado ? 'Esta empleado' : 'No esta empleado');
+//Array de cada tipo
+let nombres = ['Marcos', 'Fernando'];
+let numerosRand = [1, 32, 232];
+//Array random
+let valoresVarios = [true, 'Hola', 23, false];
+//Enums
+var Tareas;
+(function (Tareas) {
+    Tareas["Completada"] = "Completada";
+    Tareas["En proceso"] = "En proceso";
+    Tareas["Demorado"] = "Demorado";
+    Tareas["No empezado"] = "No empezado";
+})(Tareas || (Tareas = {}));
+let estadoTarea = Tareas.Completada;
+console.log(estadoTarea);
+// ** Aplicar datos a la interface
+let aplicacion = {
+    nombre: 'Tecnico en software',
+    sueldo: 2900,
+    tareas: Tareas.Completada,
+    tiempo: 9,
+    ocupado: true,
+};
+console.log('Esto es una interfase');
+console.log(aplicacion);
+console.log('----------------------------------------------------');
+let productoVisto = {
+    nombre: 'Leche la serenisima',
+    fechaDeVencimiento: '20/20/2031',
+    precio: 200,
+    disponible: true,
+};
+console.log('Esto es un type');
+console.log(productoVisto);
+//Lista de Productos:Type
+let listaDeCompra = [
+    {
+        nombre: 'Leche la serenisima',
+        fechaDeVencimiento: '20/20/2031',
+        precio: 200,
+        disponible: true,
+    },
+    {
+        nombre: 'Yogur el malisioso',
+        fechaDeVencimiento: '22/10/2010',
+        precio: 10000,
+        disponible: true,
+    },
+    {
+        nombre: 'Queso el original',
+        fechaDeVencimiento: '20/12/2300',
+        precio: 2003,
+        disponible: false,
+    },
+];
+//Lista de puestos:Inteface
+let empleados = [
+    {
+        nombre: 'Tecnico en software',
+        sueldo: 2900,
+        tareas: Tareas.Completada,
+        tiempo: 9,
+        ocupado: true
+    },
+    {
+        nombre: 'Tester',
+        sueldo: 3000,
+        tareas: Tareas.Demorado,
+        tiempo: 9,
+        ocupado: true
+    },
+    {
+        nombre: 'Q&A Asistente',
+        sueldo: 2900,
+        tareas: Tareas["En proceso"],
+        tiempo: 9,
+        ocupado: true
+    }
+];
+empleados.forEach((puestos, index) => {
+    console.log(`${index + 1} - ${puestos.tareas}`);
+});
--- TypeScript Basico/src/index.ts
+++ TypeScript Basico/src/index.ts
@@ -1,4 +1,142 @@
-console.log("Hola Nodemon");
+//Declaracion de variables
+let nombre: string = 'Marcos'
+let edad: number = 23
+let email: string = 'antonbarroso16@gmail.com'
+let empleado: boolean = false
+let comodin: any = 'Soy lo que sea' //Cambia a todo tipo de variable :Any
+
+//Console.log
+
+console.log('Hola ' + nombre + ' tienes ' + edad + ' y tu email es: ' + email)
+console.log(empleado ? 'Esta empleado' : 'No esta empleado')
+
+//Array de cada tipo
+
+let nombres: string[] = ['Marcos', 'Fernando']
+let numerosRand: number[] = [1, 32, 232]
+
+//Array random
+
+let valoresVarios: (string | number | boolean)[] = [true, 'Hola', 23, false]
+
+//Enums
+
+enum Tareas {
+  'Completada' = 'Completada',
+  'En proceso' = 'En proceso',
+  'Demorado' = 'Demorado',
+  'No empezado' = 'No empezado'
+}
+
+let estadoTarea: Tareas = Tareas.Completada
+
+console.log(estadoTarea)
+
+// ** Interface
+
+interface Puestos {
+  nombre: string
+  sueldo: number
+  tareas: Tareas
+  tiempo: number
+  ocupado: boolean
+}
+
+// ** Aplicar datos a la interface
+let aplicacion: Puestos = {
+  nombre: 'Tecnico en software',
+  sueldo: 2900,
+  tareas: Tareas.Completada,
+  tiempo: 9,
+  ocupado: true,
+}
+
+console.log('Esto es una interfase')
+console.log(aplicacion)
+console.log('----------------------------------------------------')
+
+// ** Types de typescript
+
+type Producto = {
+  nombre: string
+  fechaDeVencimiento: string
+  precio: number
+  disponible: boolean
+}
+
+let productoVisto: Producto = {
+  nombre: 'Leche la serenisima',
+  fechaDeVencimiento: '20/20/2031',
+  precio: 200,
+  disponible: true,
+}
+
+console.log('Esto es un type')
+console.log(productoVisto)
+
+//Lista de Productos:Type
+let listaDeCompra: Producto[] = [
+  {
+    nombre: 'Leche la serenisima',
+    fechaDeVencimiento: '20/20/2031',
+    precio: 200,
+    disponible: true,
+  },
+  {
+    nombre: 'Yogur el malisioso',
+    fechaDeVencimiento: '22/10/2010',
+    precio: 10000,
+    disponible: true,
+  },
+  {
+    nombre: 'Queso el original',
+    fechaDeVencimiento: '20/12/2300',
+    precio: 2003,
+    disponible: false,
+  },
+]
+
+//Lista de puestos:Inteface
+let empleados: Puestos[] = [
+    {
+        nombre: 'Tecnico en software',
+        sueldo: 2900,
+        tareas: Tareas.Completada,
+        tiempo: 9,
+        ocupado: true
+    },
+    {
+        nombre: 'Tester',
+        sueldo: 3000,
+        tareas: Tareas.Demorado,
+        tiempo: 9,
+        ocupado: true
+    },
+    {
+        nombre: 'Q&A Asistente',
+        sueldo: 2900,
+        tareas: Tareas["En proceso"],
+        tiempo: 9,
+        ocupado: true
+    }
+]
+console.log();
+
+//For clasico
+for(let i = 0; i < empleados.length; i++){
+    const empleadoArray = empleados[i];
+    console.log(`${i+1} - ${empleadoArray.nombre}`);
+}
+console.log();
+console.log("---------------------------------------");
+console.log();
 
 
+
+//ForEach
+empleados.forEach(
+  (puestos : Puestos, index : number)=>{
+    console.log(`${index+1} - ${puestos.tareas}`);
+  }
+)
 
